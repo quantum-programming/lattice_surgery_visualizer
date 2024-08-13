34c800e02771dde98649df79aecc86d07b5ded4c
hari64 <hari64boli64@gmail.com>
2024-08-13 16:16:48 +0900

    add link to github repository

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -3,9 +3,7 @@ import type { FC } from 'react';
 const Description: FC = () => (
   <div>
     <div>
-      <h1>
-        Visualizer for Lattice Surgery
-      </h1>
+      <h1>Visualizer for Lattice Surgery</h1>
       This is a visualizer for Lattice Surgery.
       <ul>
         <li>
@@ -15,6 +13,18 @@ const Description: FC = () => (
           <strong>tooltip</strong> will show up when you hover over the paths.
         </li>
       </ul>
+      GitHub repository:{' '}
+      <a href="https://github.com/quantum-programming/lattice_surgery_visualizer">
+        <img
+          style={{
+            width: '25px',
+            height: '25px',
+            verticalAlign: 'middle',
+          }}
+          src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
+          alt="GitHub"
+        />
+      </a>
     </div>
   </div>
 );
